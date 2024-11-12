d4bd02dba4258e197e544226db66277eaf715cb6
CYOSP <cyosp@cyosp.com>
2024-11-12 08:57:11 +0100

    Update build MacOS app script

--- CYOSP/build-macos-app-and-run.bash
+++ CYOSP/build-macos-app-and-run.bash
@@ -12,15 +12,16 @@ if [ -d tmp ]; then rm -r tmp; fi
 
 if [ "$MODE" = "dev" ]
 then
-    GRUNT_PARAMETERS="dev-desktop-darwin --skip-sign --dev-mode"
+    GRUNT_PARAMETERS_WEB_APP="test"
+    GRUNT_PARAMETERS_MACOS_APP="dev-desktop-darwin --skip-sign --dev-mode"
     APP_PARAMETERS="--devtools"
 else
-    GRUNT_PARAMETERS="--max-old-space-size=4096 desktop-darwin --skip-sign"
+    GRUNT_PARAMETERS_WEB_APP=""
+    GRUNT_PARAMETERS_MACOS_APP="--max-old-space-size=4096 desktop-darwin --skip-sign"
     APP_PARAMETERS=""
 fi
 
 set -e
-# Build web app
-# $ROOT/node_modules/.bin/grunt test
-$ROOT/node_modules/.bin/grunt $GRUNT_PARAMETERS
+$ROOT/node_modules/.bin/grunt $GRUNT_PARAMETERS_WEB_APP
+$ROOT/node_modules/.bin/grunt $GRUNT_PARAMETERS_MACOS_APP
 $ROOT/tmp/desktop/KeeWeb-darwin-$ARCH/KeeWeb.app/Contents/MacOS/KeeWeb $APP_PARAMETERS
